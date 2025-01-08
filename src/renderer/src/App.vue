e7a0535546aef7af62ee2dfd48117eeb78a2e511
Ckrvxr <85326108+ckrvxr@users.noreply.github.com>
2025-01-08 17:16:31 +0800

    ✨: Add vuetify

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -1,7 +1,31 @@
 <script setup lang="ts">
 const ipcHandle = () => window.electron.ipcRenderer.send('ping')
+import './assets/main.css'
+
+import { ref } from 'vue'
+
+const drawer = ref(true)
 </script>
 
 <template>
-  <button target="_blank" rel="noreferrer" @click="ipcHandle">Send IPC</button>
+  <v-layout>
+    <v-navigation-drawer v-model="drawer" floating permanent>
+      <v-list density="comfortable">
+        <v-list-item class="ma-2" link nav title="Dashboard" />
+        <v-list-item class="ma-2" link nav title="Statistics" />
+        <v-list-item class="ma-2" link nav title="Devices" />
+        <v-list-item class="ma-2" link nav title="Settings" />
+        <v-list-item class="ma-2" link nav title="Account" />
+        <v-btn @click="ipcHandle"> Send IPC </v-btn>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar border="b" class="ps-4" flat>
+      <v-app-bar-title>Dashboard</v-app-bar-title>
+    </v-app-bar>
+    <v-main>
+      <div class="pa-4">
+        <v-sheet border="dashed md" color="surface-light" height="500" rounded="lg" width="100%" />
+      </div>
+    </v-main>
+  </v-layout>
 </template>
